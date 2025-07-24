fc16c92480f50004369be86d3062d6e8bdefbb4d
Jonathan Graesser <jonathan.graesser@novatronfusion.com>
2025-07-24 16:41:38 +0200

    fixed variables

--- h5web_app/vite.config.ts
+++ h5web_app/vite.config.ts
@@ -2,5 +2,15 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      swcOptions: {
+        minify: false,
+      },
+    }),
+  ],
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
 });
